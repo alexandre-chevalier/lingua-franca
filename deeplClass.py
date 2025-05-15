e8e5f59b66c0da3520814d181a19f3a0cc34f3a4
alexandre <alexandre.chevalier@laplateforme.io>
2025-05-15 19:41:38 +0200

    try except in deeplclass.py

--- deeplClass.py
+++ deeplClass.py
@@ -8,20 +8,23 @@ class DeeplClass:
  
     def __init__(self):
         api_key = os.getenv("DEEPL_API_KEY")
+        if not api_key:
+            raise ValueError("DEEPL_API_KEY n'a pas ete trouver")
         self.translator = deepl.Translator(api_key)
     
     def retrieve_language(self):
         langage = self.translator.get_source_languages()
         ret_lang = []
-        
-        for i,j in enumerate(langage):
-            ret_lang.append({ "code": j.code, "name" : j.name})
-            
+        for j in langage:
+            ret_lang.append({ "code": j.code, "name" : j.name}) 
         return ret_lang
     
     def translate_language(self, text, lang):
-        result= self.translator.translate_text(text, target_lang=f"{lang}")
-        return result.text
+        try:
+            result= self.translator.translate_text(text, target_lang=f"{lang}")
+            return result.text
+        except Exception as e:
+            raise RuntimeError(f"Translation failed: {e}")
     
 
 
